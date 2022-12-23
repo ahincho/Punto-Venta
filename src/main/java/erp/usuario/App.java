830b537478480440f20afc9649bf6324f1a3e915
ahincho <ahincho@unsa.edu.pe>
2022-12-23 16:53:11 -0500

    Curso JavaFX

--- src/main/java/erp/usuario/App.java
+++ src/main/java/erp/usuario/App.java
@@ -5,10 +5,12 @@ package erp.usuario;
  * Hello world!
  * Maven: https://youtu.be/zlHXH6maOR0
  * Getting Started: https://youtu.be/4gnLTg8e3xs
- * Little Subject: https://youtu.be/8aDBQJPSYKY
+ * Little Maven Subject: https://youtu.be/8aDBQJPSYKY
  * Oficial Page: https://maven.apache.org/index.html
  * GUI's: https://youtu.be/AubJaosfI-0
  * GUI Test: https://github.com/TestFX/TestFX/
+ * Maven GUI Dependencies: https://youtu.be/3eE-xz-l2mY
+ * JavaFX Subject: https://youtu.be/VMOJ33m0Ooc
  */
 
 public class App 
