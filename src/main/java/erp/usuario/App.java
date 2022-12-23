b471449744344e867e28fe2d03d17d42482e8159
ahincho <ahincho@unsa.edu.pe>
2022-12-23 16:45:23 -0500

    Agregando Dependencias a JavaFX

--- src/main/java/erp/usuario/App.java
+++ src/main/java/erp/usuario/App.java
@@ -4,6 +4,11 @@ package erp.usuario;
 /**
  * Hello world!
  * Maven: https://youtu.be/zlHXH6maOR0
+ * Getting Started: https://youtu.be/4gnLTg8e3xs
+ * Little Subject: https://youtu.be/8aDBQJPSYKY
+ * Oficial Page: https://maven.apache.org/index.html
+ * GUI's: https://youtu.be/AubJaosfI-0
+ * GUI Test: https://github.com/TestFX/TestFX/
  */
 
 public class App 
